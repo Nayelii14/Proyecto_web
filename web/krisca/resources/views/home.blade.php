ee9e81d51a5059a6f6e478def44a7a235be15175
Nayeli Huerta <nayeli.14.nh@gmail.com>
2021-07-30 21:35:27 -0400

    Creación estructura de página

---
+++ web/krisca/resources/views/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.master')
+
+@section('body')
+    <h1>Hola</h1>
+@endsection
---
+++ web/krisca/resources/views/inicio_sesion.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.master')
+
+@section('body')
+    <h1>Inicio sesion</h1>
+@endsection
